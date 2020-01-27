bd4a270ad4e104d154802eceb7f384b671498ac0
Chad Kidd <chadkidd0@gmail.com>
2020-01-27 11:25:28 -0800

    initial commit

---
+++ TestARM/src/TestARM.c
@@ -0,0 +1,19 @@
+/*
+ ============================================================================
+ Name        : TestARM.c
+ Author      : Chad
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	int x = 5;
+	x++;
+	puts("!!!Hello World ARM!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
